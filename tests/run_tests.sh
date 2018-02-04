2c242f0fd49aff24be6f15998cd98ef74a0df199
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2018-02-04 12:54:43 -0800

    simulate pid and motion magic algorithms

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,8 +3,9 @@
 cd $(dirname $0)
 
 if [ "$RUNCOVERAGE" == "1" ]; then
-    python3 -m coverage run --source ctre -m pytest "$@"
-    python3 -m coverage report -m
+    #python3 -m coverage run --source ctre -m pytest --cov-report html --cov-report term --cov ctre "$@"
+    python3 -m pytest --cov-report html --cov-report term --cov ctre "$@"
+    #python3 -m coverage report -m
 else
     python3 -m pytest "$@"
 fi
