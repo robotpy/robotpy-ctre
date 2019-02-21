9c18e7ff1fee96d3ee80a5ce320e776e63ad90d5
Steven J. Owens <stevenjowens@gmail.com>
2019-02-20 20:15:34 -0500

    changed ctre_lib_version back to 5.14.0, merged the if ctre_rpi section back into the roborio section.

--- setup.py
+++ setup.py
@@ -12,19 +12,18 @@ import subprocess
 import sys
 import setuptools
 
-ctre_lib_version = "5.13.0"
+ctre_lib_version = "5.14.0"
 
 setup_dir = dirname(__file__)
 git_dir = join(setup_dir, ".git")
 base_package = "ctre"
 version_file = join(setup_dir, base_package, "version.py")
 
-# Check if we're being invoked on a raspberrypi.
 architecture = "athena"
+# Check if we're being invoked on a raspberrypi.
 ctre_rpi = os.environ.get("CTRE_RPI")
 if ctre_rpi:
     architecture = "raspbian"
-
 
 # Automatically generate a version.py based on the git version
 if exists(git_dir):
@@ -237,32 +236,16 @@ get = Downloader()
 _travis_build = os.environ.get("TRAVIS_BUILD")
 libraries = None
 cmdclass = {}
-if ctre_rpi:
-    ext_modules = [
-        Extension(
-            "ctre._impl.ctre_cpp",
-            ["ctre/_impl/ctre_cpp.cpp"],
-            include_dirs=[
-                # Path to pybind11 headers
-                get_pybind_include(),
-                get_pybind_include(user=True),
-                get.ctresrc,
-            ],
-            libraries=libraries,
-            library_dirs=[
-                join(get.ctresrc, "linux", "athena", "static"),
-            ],
-            language="c++",
-        )
-    ]
-    
-# Detect roboRIO.. not foolproof, but good enough
-elif exists("/etc/natinst/share/scs_imagemetadata.ini") or _travis_build:
+# Detect raspberrypi or roboRIO.. not foolproof, but good enough
+if exists("/etc/natinst/share/scs_imagemetadata.ini") or _travis_build or ctre_rpi:
 
     # Don't try to link when testing on travis, as it will fail
     # -> We can still catch compile errors, which is good enough I suspect
-    if not _travis_build:
-        libraries = ["wpiHal", "CTRE_PhoenixCCI"]
+    if not _travis_build: 
+        if ctre_rpi:
+            libraries = ["CTRE_PhoenixCCI"]
+        else:
+            libraries = ["wpiHal", "CTRE_PhoenixCCI"]
 
     ext_modules = [
         Extension(
@@ -276,17 +259,18 @@ elif exists("/etc/natinst/share/scs_imagemetadata.ini") or _travis_build:
             ],
             libraries=libraries,
             library_dirs=[
-                join(get.halsrc, "linux", "athena", "shared"),
-                join(get.ctresrc, "linux", "athena", "static"),
+                join(get.halsrc, "linux", architecture, "shared"),
+                join(get.ctresrc, "linux", architecture, "static"),
             ],
             language="c++",
         )
     ]
 
-    # This doesn't actually work, as it needs to be installed before setup.py is ran
-    # ... but we specify it
-    # install_requires = ['pybind11>=1.7']
-    install_requires.append("robotpy-hal-roborio>=2019.0.0,<2020.0.0")
+    if not ctre_rpi:
+        # This doesn't actually work, as it needs to be installed before setup.py is ran
+        # ... but we specify it
+        # install_requires = ['pybind11>=1.7']
+        install_requires.append("robotpy-hal-roborio>=2019.0.0,<2020.0.0")
     cmdclass = {"build_ext": BuildExt}
 else:
     install_requires.append("robotpy-hal-sim>=2019.0.0,<2020.0.0")
